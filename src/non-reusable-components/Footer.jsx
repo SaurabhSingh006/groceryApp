b4832a98065fd50e9138a81d861e46fc86140a86
Saurabh Singh <saurabhsinghsix@gmail.com>
2023-11-05 20:06:20 +0530

    Update home page

--- src/non-reusable-components/Footer.jsx
+++ src/non-reusable-components/Footer.jsx
@@ -1,7 +1,8 @@
 import { IoHomeOutline, IoHome } from "react-icons/io5";
 import { TbCategory, TbCategoryFilled } from "react-icons/tb";
 import { FaRegListAlt, FaListAlt } from "react-icons/fa";
-import { BsPerson, BsFillPersonFill } from "react-icons/bs";
+import { BsFillPersonFill } from "react-icons/bs";
+import { MdPersonOutline } from "react-icons/md"
 import { Link, useLocation } from "react-router-dom";
 
 function Footer() {
@@ -10,38 +11,38 @@ function Footer() {
   return (
     <div className="grid grid-cols-4 place-content-center p-4 absolute bottom-0 w-full drop-shadow-2xl bg-white">
       <Link to="/home">
-        {
-          pathname === "/home" ?
-          <IoHome className="text-2xl text-greenHome1 m-auto" /> :
+        {pathname === "/home" ? (
+          <IoHome className="text-2xl text-greenHome1 m-auto" />
+        ) : (
           <IoHomeOutline
             className="text-2xl m-auto"
             style={{ fontWeight: "500" }}
-          /> 
-        }
+          />
+        )}
       </Link>
 
       <Link to="/home/category">
-        {
-          pathname === "/home/category" ?
-          <TbCategoryFilled className="text-2xl text-greenHome1 m-auto" /> :
+        {pathname === "/home/category" ? (
+          <TbCategoryFilled className="text-2xl text-greenHome1 m-auto" />
+        ) : (
           <TbCategory className="text-2xl m-auto" />
-        }
+        )}
       </Link>
 
       <Link to="/home/order">
-        {
-          pathname === "/home/order" ?
-          <FaListAlt className="text-2xl text-greenHome1 m-auto" /> : 
+        {pathname === "/home/order" ? (
+          <FaListAlt className="text-2xl text-greenHome1 m-auto" />
+        ) : (
           <FaRegListAlt className="text-2xl m-auto" />
-        }
+        )}
       </Link>
 
       <Link to="/home/account">
-        {
-          pathname === "/home/account" ? 
-          <BsFillPersonFill className="text-2xl text-greenHome1 m-auto" /> :
-          <BsPerson className="text-2xl m-auto" />
-        }
+        {pathname === "/home/account" ? (
+          <BsFillPersonFill className="text-2xl text-greenHome1 m-auto" />
+        ) : (
+          <MdPersonOutline className="text-2xl m-auto" />
+        )}
       </Link>
     </div>
   );
